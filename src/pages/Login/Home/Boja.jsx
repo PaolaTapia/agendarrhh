1d49b3a66932baecba957595907d25a346e9e14e
gabriel.rosales <gabriel.rosales@zentricx.com>
2022-05-31 17:22:59 -0300

    commit

--- src/pages/Login/Home/Boja.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Boja = () => {
-  return (
-    <div>Boja</div>
-  )
-}
-
-export default Boja
--- src/router/router.js
+++ src/router/router.js
@@ -2,7 +2,6 @@ import React from "react";
 import {
   BrowserRouter as Router,useRoutes
 } from "react-router-dom";
-import Boja from "../pages/Login/Home/Boja";
 import Home from "../pages/Login/Home/Home";
 
 
@@ -12,7 +11,6 @@ const Routers = () => {
   const AppRouter = () => {
     let routes = useRoutes([
       { path: "/", element: <Home /> },
-      { path: "/boca", element: <Boja /> },
     ]);
     return routes;
   };
